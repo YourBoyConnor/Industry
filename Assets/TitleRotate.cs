046825df9dbe6021bbd74a67d838a32cc7e0d01c
YourBoyConnor <33465131+YourBoyConnor@users.noreply.github.com>
2018-02-25 15:50:39 -0500

    Assets

---
+++ Assets/TitleRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, 0, -1);
+	}
+
+    public void Play()
+    {
+
+        SceneManager.LoadSceneAsync("Industry");
+        
+    }
+
+}
